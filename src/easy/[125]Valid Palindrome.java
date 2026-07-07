7fd2df4fbaeaaa5b7fcb8160fef5ab2df4a8a197
LDKhangg <lekhangmc12@gmail.com>
2026-07-07 14:01:46 +0700

    2026-07-07 — update solutions

---
+++ src/easy/[125]Valid Palindrome.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=125 lang=java
+ *
+ * [125] Valid Palindrome
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(String s) {
+    s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+
+        int l = 0;
+        int r = s.length() - 1;
+
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
+// @lc code=end
+
