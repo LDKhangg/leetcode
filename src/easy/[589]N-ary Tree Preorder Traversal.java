0d2d78be1d94ecca5a8f34216d42d0412bac6d7e
LDKhangg <lekhangmc12@gmail.com>
2026-07-09 22:47:39 +0700

    2026-07-09 — update solutions

---
+++ src/easy/[589]N-ary Tree Preorder Traversal.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=589 lang=java
+ *
+ * [589] N-ary Tree Preorder Traversal
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(res,root);
+        return res;
+    }
+    private void dfs(List<Integer> res ,Node root){
+        if(root == null) return;
+        res.add(root.val);
+        for(Node n : root.children){
+            dfs(res,n);
+        }
+    }
+}
+// @lc code=end
+
---
+++ src/easy/[590]N-ary Tree Postorder Traversal.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode id=590 lang=java
+ *
+ * [590] N-ary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(res,root);
+        return res;
+    }
+
+    private void dfs(List<Integer> res ,Node root){
+        if(root==null) return;
+        for(Node n:root.children){
+            dfs(res,n);
+        }
+        res.add(root.val);
+    }
+}
+// @lc code=end
+
