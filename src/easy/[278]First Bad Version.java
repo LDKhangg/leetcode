cbc938564ce988404dcbf688059a1e8734cce194
LDKhangg <lekhangmc12@gmail.com>
2026-07-11 23:45:22 +0700

    2026-07-11 — update solutions

---
+++ src/easy/[278]First Bad Version.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=278 lang=java
+ *
+ * [278] First Bad Version
+ */
+
+// @lc code=start
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1,r=n;
+        while(l<r){
+            int m = l+(r-l)/2;
+            if(isBadVersion(m)){
+                r=m;
+            } else{
+                l=m+1;
+            }
+        }
+        return l;
+    }
+
+}
+// @lc code=end
+
