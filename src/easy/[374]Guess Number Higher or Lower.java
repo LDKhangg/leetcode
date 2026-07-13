751bea090199e997e5d4b9fc98b628e1f98ff318
LDKhangg <lekhangmc12@gmail.com>
2026-07-13 14:09:19 +0700

    2026-07-13 — update solutions

---
+++ src/easy/[374]Guess Number Higher or Lower.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=374 lang=java
+ *
+ * [374] Guess Number Higher or Lower
+ */
+
+// @lc code=start
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l=1;
+        int r= n;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(guess(m)==0) {
+                return m;
+            }
+            else if(guess(m)==-1){
+                r=m-1;
+            } else{
+                l=m+1;
+            }
+        }
+        return r;
+    }
+}
+// @lc code=end
+
