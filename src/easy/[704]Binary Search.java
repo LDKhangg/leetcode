dcd46de2d4602a53f8e91ee40d458945697a4215
LDKhangg <lekhangmc12@gmail.com>
2026-07-13 14:47:26 +0700

    2026-07-13 — update solutions

---
+++ src/easy/[704]Binary Search.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=704 lang=java
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            int m =l+ (r-l)/2;
+            if(nums[m]==target){
+                return m;
+            }
+            else if(nums[m]<target){
+                l=m+1;
+            } else{
+                r=m-1;
+            }
+        }
+        return -1;
+    }
+}
+// @lc code=end
+
