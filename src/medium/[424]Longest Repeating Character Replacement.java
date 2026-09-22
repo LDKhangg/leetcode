6fa48dd3c3c1b92278064662e7e9e775e69f7566
LDKhangg <lekhangmc12@gmail.com>
2026-09-22 20:56:17 +0700

    2026-09-22 — update solutions

---
+++ src/medium/[424]Longest Repeating Character Replacement.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=424 lang=java
+ *
+ * [424] Longest Repeating Character Replacement
+ */
+
+// @lc code=start
+class Solution {
+    public int characterReplacement(String s, int k) {
+        char[] cArr = s.toCharArray();
+        int[] count = new int[26];
+
+        int left = 0, maxFreq = 0;
+        for (int right = 0; right < cArr.length; right++) {
+            int currIdx = cArr[right] - 'A';
+            count[currIdx]++;
+            if (maxFreq < count[currIdx]) {
+                maxFreq = count[currIdx];
+            }
+            if (right - left + 1 - k > maxFreq) {
+                count[cArr[left] - 'A'] -= 1;
+                left++;
+            }
+
+        }
+        return cArr.length - left;
+    }
+}
+// @lc code=end
